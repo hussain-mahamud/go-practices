5a5595141fe2cb0eb459a2175cabd3bc32667017
Hussain Mahamud <hussainmahamud.swe@gmail.com>
2025-04-13 10:53:23 +0600

    wip

--- main.go
+++ main.go
@@ -2,4 +2,11 @@ package main
 import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
+	getPersonalDetails()
+
+}	
+func getPersonalDetails(){
+	fmt.Println("Name: John Doe")
+	fmt.Println("Age: 30")
+	fmt.Println("Occupation: Software Developer")
 }
